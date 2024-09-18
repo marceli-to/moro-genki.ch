019b90d5c46dc876108122711da71f540dc77e18
Marcel Stadelmann <m@marceli.to>
2024-09-18 17:24:04 +0200

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'Wir machen Geschichten. Panda & Pinguin ist die Agentur für zeitgemässe Kommunikation.'
+  'meta_description' => 'Integrative Medizin und Prävention, Allgemeine Medizin (D) FMH, Traditionelle Japanische Medizin, Harvard Style Akupunktur und YNSA, Rimkus® Hormon-Therapie, Medizinische LASER-und Licht-Therapie SGML"><meta property="og:image" content="https://moro-genki.ch/fileadmin/assets/img/og.jpg"><meta property="og:title" content="Home - Moro Genki Clinic"><meta property="og:description" content="Integrative Medizin und Prävention, Allgemeine Medizin (D) FMH, Traditionelle Japanische Medizin, Harvard Style Akupunktur und YNSA, Rimkus® Hormon-Therapie, Medizinische LASER-und Licht-Therapie SGML'
 ];
